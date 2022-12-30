b20c36add2ad64e5d1c0bdba50915b67e6b9bc24
William Colvill <89409681+Colvill44@users.noreply.github.com>
2022-12-29 23:11:35 -0500

    Add files via upload

---
+++ Brand/scripts/form.js
@@ -0,0 +1,6 @@
+function check() {
+	document.getElementById("namevalue").innerHTML = document.contact.name.value;
+	document.getElementById("phonevalue").innerHTML = document.contact.phone.value;
+	document.getElementById("emailvalue").innerHTML = document.contact.email.value;
+	document.getElementById("qcvalue").innerHTML = document.contact.qc.value;
+}
---
+++ clean/scripts/slides.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $("#slider").bxSlider({
+        auto: true,
+        minSlides: 1,
+        maxSlides: 1,
+        slideWidth: 1000,
+		slideHeight: 650,
+        slideMargin: 20,
+		randomStart: true,
+		captions: true,
+		speed: 500
+    });
+});
